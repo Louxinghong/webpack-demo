e00224640b24d4ccc6ea4ce1dd4965280bf284c9
louxinghong <louxinghong@xinyunlian.com>
2020-01-20 15:29:55 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
+const glob = require("glob");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //打包html的插件
-
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //打包css插件
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // 清理dist文件内容
 
+glob("./src/components/**/*.js", function(er, files) {
+  console.log(files);
+  return files;
+});
 module.exports = {
   entry: {
     content: "./src/js/content.js",
@@ -23,16 +28,18 @@ module.exports = {
     ]
   },
   plugins: [
+    // 清理dist文件内容
+    new CleanWebpackPlugin(),
     // html打包插件
     new HtmlWebpackPlugin({
       chunks: ["form", "index"],
       template: "./src/html/form-show.html",
-      filename: "form.html"
+      filename: "html/form.html"
     }),
     new HtmlWebpackPlugin({
       chunks: ["content", "index"],
       template: "./src/html/show-content.html",
-      filename: "qr-content.html"
+      filename: "html/qr-content.html"
     }),
 
     // css打包插件
